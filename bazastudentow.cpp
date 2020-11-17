4cd134cf1b21d4af552fbe4e9db5c111d17a6288
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-17 11:51:15 +0100

    first commit on new repo to database with std

---
+++ bazastudentow.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <fstream>
+
+using namespace std;
+
+struct Osoba{
+
+    string imie;
+    string nazwisko;
+    int nrAlbumu;
+    float srOcen;
+    int wiek;
+};
+
+int main(){
+
+fstream plik;
+
+plik.open( "studenci.txt", ios::out | ios::app);
+
+Osoba uczniowie[5];
+
+for(int i = 0; i < 5; i++){
+    cout << "podaj imie i nazwisko " << i+1 << " ucznia: " ;
+    cin >> uczniowie[i].imie;
+    cin >> uczniowie[i].nazwisko;
+}
+
+cout << endl;
+
+for(int i = 0; i < 5; i++){
+    cout << "podaj numer albumu " << uczniowie[i].imie <<" "<< uczniowie[i].nazwisko <<" ";
+    cin >> uczniowie[i].nrAlbumu;
+}
+
+cout << endl;
+
+for(int i = 0; i < 5; i++){
+    cout << "podaj wiek " << uczniowie[i].imie <<" "<< uczniowie[i].nazwisko <<" ";
+    cin >> uczniowie[i].wiek;
+}
+
+cout << endl;
+
+for(int i = 0; i < 5; i++){
+    cout << "podaj srednia ocen: " << uczniowie[i].imie <<" "<< uczniowie[i].nazwisko <<" ";
+    cin >> uczniowie[i].srOcen;
+}
+
+cout << endl;
+
+for(int i = 0; i < 5; i++){
+plik << uczniowie[i].imie <<" "<< uczniowie[i].nazwisko <<" "<< uczniowie[i].wiek <<" "<< uczniowie[i].nrAlbumu <<" "<< uczniowie[i].srOcen << endl;
+} 
+
+
+plik.close();
+
+plik.open( "studenci.txt", ios::in );
+
+
+if(plik.is_open())
+	{
+		char wiersz[10000];
+
+		while(plik.getline(wiersz,10000)) 
+		{
+			cout<< wiersz << endl;
+			
+		}
+        cout << endl;
+	}
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
