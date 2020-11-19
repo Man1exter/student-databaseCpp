76e0fcb6e8b52016f1632ee74d427bfe5eb82c68
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-19 18:03:49 +0100

    change spaces with a lot of bugs in comment line/s

--- bazastudentow.cpp
+++ bazastudentow.cpp
@@ -77,12 +77,12 @@ if(plik.is_open())
 
 }
 
-//3) Następnie program umożliwi wybór następujących funkcji poprzez naciśnięcieodpowiedniego przycisku na klawiaturze:
+//3) Następnie program umożliwi wybór następujących funkcji poprzez naciśnięcie odpowiedniego przycisku na klawiaturze:
 
 //<L> wylistowanie nazwisk, imion i wieku wszystkich studentów ​w porządku alfabetycznymoraz zamiana wszystkich liter na duże;
 
 //<D> dodanie nowego studenta (ale nie więcej niż 20) oraz wprowadzenie wszystkich cech go
-//opisujących; sprawdzone zostanie czy imię studenta znajduje się na predefiniowanej liściedopuszczalnych imion (zbiór ze słownikiem imion jako stała);
+//opisujących; sprawdzone zostanie czy imię studenta znajduje się na predefiniowanej liście dopuszczalnych imion (zbiór ze słownikiem imion jako stała);
 
 //<U> usunięcie studenta ze średnią ocen poniżej 2,5;
 
