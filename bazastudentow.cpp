a0534724cb894fdb7972c9cbc04fd03c5cd8dc19
Mariusz Perzynski <m.perzyk@interia.pl>
2020-12-06 19:14:10 +0100

    show list with students after exit

--- bazastudentow.cpp
+++ bazastudentow.cpp
@@ -189,17 +189,20 @@ cout << endl;
 
         case KEY_U : //usuniecie z srocen 2.5 
 
-        for(int i = 0; i < liczbStudentow + nowe; i++){
+        for(int i = 0; i < 1; i++){
             if(uczniowie[i].srOcen >= 2.5){
 
-              cout << "osoba ktora zostanie usunieta to: " << uczniowie[i].imie <<" "<< uczniowie[i].nazwisko <<" ma lat "<< uczniowie[i].wiek <<" numer albumu: "<< uczniowie[i].nrAlbumu <<" oraz średnia ocen to: "<< uczniowie[i].srOcen << endl;
+             cout << "Aktualnie nie ma osoby ktora ma srednia mniejsza lub rowna 2.5" << endl;
+             cout << endl;
+
+            } else {
+              cout << "Osoba ktora zostanie usunieta to: " << uczniowie[i].imie <<" "<< uczniowie[i].nazwisko <<" ma lat "<< uczniowie[i].wiek <<" numer albumu: "<< uczniowie[i].nrAlbumu <<" oraz średnia ocen to: "<< uczniowie[i].srOcen << endl;
              
                               cout << endl;
 
-                              
-
+                
                               cout << endl;
-             cout << "Zostala usunieta osoba z srednia ocen 2.5 lub mniejsza" << " " << uczniowie[i].imie << "   "<< uczniowie[i].nazwisko << endl;
+             cout << "Zostala usunieta osoba z srednia ocen 2.5 lub mniejsza" << endl;
                           cout<<endl;
         }
     }
@@ -220,7 +223,15 @@ cout << endl;
 
         default: //wyjscie
         cout << "Aktualnie skorzystales z dziennika studentow, milego dnia :)" << endl;
-        exit(1);
+        cout << endl;
+
+        cout << "Twoja lista uczniow wyglada teraz tak: " << endl;
+        cout << endl;
+
+        for(int i = 0; i < liczbStudentow + nowe - 1; i++){
+        cout << uczniowie[i].imie <<" "<< uczniowie[i].nazwisko <<" ma lat "<< uczniowie[i].wiek <<" numer albumu: "<< uczniowie[i].nrAlbumu <<" oraz średnia ocen to: "<< uczniowie[i].srOcen << endl;
+} 
+        
     }
 }
 
